f8d0edbda35e296b5d88739044f444b01a21b92d
Devin Feemster <devinfeemster@gmail.com>
2019-10-08 14:50:14 -0600

    Added validation requirements for country resource at the entity level

--- FMA.API/Entities/Country.cs
+++ FMA.API/Entities/Country.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace FMA.API.Entities
 {
     public class Country
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [MaxLength(100)]
         public string Name { get; set; }
         public Capital Capital { get; set; }
+
+        [Required]
         public Guid? CapitalId { get; set; }
         public Currency Currency { get; set; }
+
+        [Required]
         public Guid? CurrencyId { get; set; }
+
+        [MaxLength(5)]
         public int Founded { get; set; }
+
+        [MaxLength(100)]
         public int Population { get; set; }
+
         public string Government { get; set; }
         [ForeignKey("GovernorId")]
         public Character Governor { get; set; }
+
+        [Required]
         public Guid? GovernorId { get; set; }
         public Nationality Nationality { get; set; }
+
+        [Required]
         public Guid? NationalityId { get; set; }
 
         public IEnumerable<Character> Members { get; set; } = new List<Character>();
---
+++ FMA.API/Migrations/20191008204930_CountyRequirements.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FMA.API.Migrations
+{
+    public partial class CountyRequirements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Currencies_CurrencyId",
+                table: "Countries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Characters_GovernorId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_CurrencyId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_NationalityId",
+                table: "Countries");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NationalityId",
+                table: "Countries",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "GovernorId",
+                table: "Countries",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CurrencyId",
+                table: "Countries",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CapitalId",
+                table: "Countries",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries",
+                column: "CapitalId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CurrencyId",
+                table: "Countries",
+                column: "CurrencyId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_NationalityId",
+                table: "Countries",
+                column: "NationalityId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Currencies_CurrencyId",
+                table: "Countries",
+                column: "CurrencyId",
+                principalTable: "Currencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Characters_GovernorId",
+                table: "Countries",
+                column: "GovernorId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Currencies_CurrencyId",
+                table: "Countries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Countries_Characters_GovernorId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_CurrencyId",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_NationalityId",
+                table: "Countries");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NationalityId",
+                table: "Countries",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "GovernorId",
+                table: "Countries",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CurrencyId",
+                table: "Countries",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CapitalId",
+                table: "Countries",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CapitalId",
+                table: "Countries",
+                column: "CapitalId",
+                unique: true,
+                filter: "[CapitalId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CurrencyId",
+                table: "Countries",
+                column: "CurrencyId",
+                unique: true,
+                filter: "[CurrencyId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_NationalityId",
+                table: "Countries",
+                column: "NationalityId",
+                unique: true,
+                filter: "[NationalityId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Currencies_CurrencyId",
+                table: "Countries",
+                column: "CurrencyId",
+                principalTable: "Currencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Countries_Characters_GovernorId",
+                table: "Countries",
+                column: "GovernorId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
