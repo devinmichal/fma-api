b47deda2532ca98c8c104711381e38fe38e0684a
Devin Feemster <devinfeemster@gmail.com>
2019-10-08 12:08:59 -0600

    validation rules for capital at the entity level

--- FMA.API/Entities/Capital.cs
+++ FMA.API/Entities/Capital.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FMA.API.Entities
 {
     public class Capital
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [MaxLength(1000)]
         public string Description { get; set; }
         public Country Country { get; set; }
     }
